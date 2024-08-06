326c77ea083bf9b2bca2abf95dbc29ebba71fb3d
Jacob <123994795+JacobTheCreature@users.noreply.github.com>
2024-08-06 11:19:37 -0800

    transfer old app code

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './modules/header/header.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'coolgames';
 }
