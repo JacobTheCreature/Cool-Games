94c78b03e762322f30ffefcaab723ea6b90efe74
Jacob <123994795+JacobTheCreature@users.noreply.github.com>
2024-08-19 10:59:05 -0800

    Fix form validation and clearing fields

--- database/sql_scripts/create_mock_data.sql
+++ database/sql_scripts/create_mock_data.sql
@@ -5,3 +5,45 @@ SELECT * FROM create_game(
     'https://www.pacogames.com/webgl/pixel-warfare-4/index.php',
     'https://images.crazygames.com/pixel-warfare_16x9/20231106041100/pixel-warfare_16x9-cover?auto=format%2Ccompress&q=45&cs=strip&ch=DPR&w=1200&h=630&fit=crop'
 );
+
+SELECT * FROM create_game(
+    'Pixel Warfare',
+    'https://www.pacogames.com/webgl/pixel-warfare-4/index.php',
+    'https://images.crazygames.com/pixel-warfare_16x9/20231106041100/pixel-warfare_16x9-cover?auto=format%2Ccompress&q=45&cs=strip&ch=DPR&w=1200&h=630&fit=crop'
+);
+
+SELECT * FROM create_game(
+    'Pixel Warfare',
+    'https://www.pacogames.com/webgl/pixel-warfare-4/index.php',
+    'https://images.crazygames.com/pixel-warfare_16x9/20231106041100/pixel-warfare_16x9-cover?auto=format%2Ccompress&q=45&cs=strip&ch=DPR&w=1200&h=630&fit=crop'
+);
+
+SELECT * FROM create_game(
+    'Pixel Warfare',
+    'https://www.pacogames.com/webgl/pixel-warfare-4/index.php',
+    'https://images.crazygames.com/pixel-warfare_16x9/20231106041100/pixel-warfare_16x9-cover?auto=format%2Ccompress&q=45&cs=strip&ch=DPR&w=1200&h=630&fit=crop'
+);
+
+SELECT * FROM create_game(
+    'Pixel Warfare',
+    'https://www.pacogames.com/webgl/pixel-warfare-4/index.php',
+    'https://images.crazygames.com/pixel-warfare_16x9/20231106041100/pixel-warfare_16x9-cover?auto=format%2Ccompress&q=45&cs=strip&ch=DPR&w=1200&h=630&fit=crop'
+);
+
+SELECT * FROM create_game(
+    'Pixel Warfare',
+    'https://www.pacogames.com/webgl/pixel-warfare-4/index.php',
+    'https://images.crazygames.com/pixel-warfare_16x9/20231106041100/pixel-warfare_16x9-cover?auto=format%2Ccompress&q=45&cs=strip&ch=DPR&w=1200&h=630&fit=crop'
+);
+
+SELECT * FROM create_game(
+    'Pixel Warfare',
+    'https://www.pacogames.com/webgl/pixel-warfare-4/index.php',
+    'https://images.crazygames.com/pixel-warfare_16x9/20231106041100/pixel-warfare_16x9-cover?auto=format%2Ccompress&q=45&cs=strip&ch=DPR&w=1200&h=630&fit=crop'
+);
+
+SELECT * FROM create_game(
+    'Pixel Warfare',
+    'https://www.pacogames.com/webgl/pixel-warfare-4/index.php',
+    'https://images.crazygames.com/pixel-warfare_16x9/20231106041100/pixel-warfare_16x9-cover?auto=format%2Ccompress&q=45&cs=strip&ch=DPR&w=1200&h=630&fit=crop'
+);
